45bb42c4661f1de0849cdf165d8fee9266eb8272
Don carderms <103117597+DonCarderms@users.noreply.github.com>
2023-06-29 09:22:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-const getHours = () => {
-         this.date = new Date()
-         this.hours = this.date.getHours()
-         this.minutes = this.date.getMinutes()
-         this.seconds = this.date.getSeconds()
-        
-        return {
-            hour : hours < 10 ? `0${hours}` : hours,
-            minute : minutes < 10 ? `0${minutes}` : minutes,
-            second : seconds < 10 ? `0${seconds}` : seconds,
-        }
-}
+const Relogio = {
+  loader: document.querySelector(".loader"),
+  digital: document.querySelector(".digital"),
+  hoursBar: document.querySelector(".hours"),
+  init() {
+    Relogio.loader.style.display = "none";
+    setInterval(Relogio.moverPonteirosRelogio, 1000);
+  },
+  getHours() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return {
+      hour: hours < 10 ? `0${hours}` : hours,
+      minute: minutes < 10 ? `0${minutes}` : minutes,
+      second: seconds < 10 ? `0${seconds}` : seconds
+    };
+  },
+  moverPonteirosRelogio() {
+    const { hour, minute, second } = Relogio.getHours();
 
-const createRelogio = () => {
-     this.loader = document.querySelector('.loader')
-     this.digital = document.querySelector('.digital')
-     this.hoursBar = document.querySelector('.hours')
-     this.loader.style.display = 'none'
+    Relogio.digital.innerHTML = `<p>${hour}h ${minute}m ${second}s</p>`;
 
-     this.moverPonteirosRelogio =  () => {
-        const { hour, minute, second } = getHours()
-    
-        digital.innerHTML = `<p>${hour}h ${minute}m ${second}s</p>`
-    
-        this.loader.style.display = 'block'
-        this.loader.style.setProperty('--loaderBeforeTransform', `rotate(${second * 6}deg)`)
-        this.loader.style.setProperty('--loaderAftereTransform', `rotate(${minute * 6}deg)`)
-        this.hoursBar.style.setProperty('--loaderHoursTransform', `rotate(${hour * 30}deg)`)
-    }
-
-    return{
-        moverPonteirosRelogio,
-    }
-}
-const reloge = createRelogio()
-setInterval(reloge.moverPonteirosRelogio, 1000);
+    Relogio.loader.style.display = "block";
+    Relogio.loader.style.setProperty(
+      "--loaderBeforeTransform",
+      `rotate(${second * 6}deg)`
+    );
+    Relogio.loader.style.setProperty(
+      "--loaderAftereTransform",
+      `rotate(${minute * 6}deg)`
+    );
+    Relogio.hoursBar.style.setProperty(
+      "--loaderHoursTransform",
+      `rotate(${hour * 30}deg)`
+    );
+  }
+};
+Relogio.init();
